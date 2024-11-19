b173fdbf2c2dcd60bf8fd90fd5a51f2368003600
root <root@Isaac>
2024-11-18 19:06:29 -0500

    added Dictionary Mem

--- WordList_1000/GenerateMem.py
+++ WordList_1000/GenerateMem.py
@@ -1,16 +1,48 @@
-
+def padToEight(input):
+    length = (8-len(input))
+    return '0'*length + input
 dict=[]
 # Open the file in read mode
 with open('1-1000.txt', 'r') as file:
     # Read each line in the file
     for line in file:
         # Print each line
-        dict.append(line.strip().toLowerCase())
+        dict.append(line.strip().lower())
         
 with open('dictionary.mem', 'w') as file:
+    current_line = ''
     for word in dict:
-        result_line
+        # escaping start
+        if len(current_line) == 24:
+            file.write(current_line + "00000002"+ "\n")
+            current_line = ""
+        elif len(current_line) == 32:
+            file.write(current_line+"\n")
+            current_line = "00000002"
+        else:
+            current_line = current_line + "00000002"
+        
+        
         word_length = len(word)
         
         for character in word:
-            binary_formatted_char = format(ord(character), 'b')
+            print(current_line, len(current_line))
+            assert len(current_line) < 32
+            binary_formatted_char = padToEight(format(ord(character), 'b'))
+            assert len(binary_formatted_char) == 8
+            current_line += str(binary_formatted_char)
+            if len(current_line) == 32:
+                file.write(current_line + "\n")
+                current_line = ""
+        assert len(current_line) < 32
+        if len(current_line) == 32:
+            file.write(current_line + "\n")
+            current_line = "00000003"
+        else:
+            current_line = current_line + "00000003"
+    if current_line!= "":
+        file.write(current_line + '0'*(32-len(current_line)))
+        #file.write(resultant_string + "\n")
+
+
+        
