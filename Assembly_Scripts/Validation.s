4b0e339734897e24b1e70a6f0781ec98df612ede
chikubidestoyer <isaac.yang@qq.com>
2024-12-07 17:37:03 -0500

    Validation Debug: Feels like all fixed?

--- Assembly_Scripts/Validation.s
+++ Assembly_Scripts/Validation.s
@@ -20,6 +20,12 @@ SETUP_VALIDATION:
     sw $t0, 3($a0)
     addi $t0, $zero, 116 # t
     sw $t0, 4($a0)
+    addi $t0, $zero, 104 # h
+    sw $t0, 5($a0)
+    addi $t0, $zero, 32 # \s
+    sw $t0, 6($a0)
+
+
 
     addi $sp, $zero, 1000
 
@@ -34,16 +40,84 @@ Validation:
     addi $t4, $zero, 0           # Pointer to dictionary m_word
     addi $t5, $t2, 0             # Pointer to character of current decrypted d_word
     addi $t8, $t4, 1             # Pointer to current character of d_word in dictionary
+    addi $a2, $zero, 0           # indicates matching began/matching complete (input string when empty also symbolizes completeness)
     j read_next_word
 
 read_next_word: # memory word
     
     lw $t3, 0($t5)               # Load the next word from the input string
-    addi $r21, $t3, 0            # for debugging purposes, stores the last character read from input string
     bne $t3, $zero, process_char # If not null, process character
+    bne $a2, $zero, reflect_unfinished_matching
     j compute_result             # If null, compute result
 
-process_char:
+reflect_unfinished_matching:
+    addi $r20, $r20, 1             # for debugging purposes
+    lwDict $t6, $t4, 0
+    nop
+    nop
+    addi $sp, $sp, -8
+    sw $t6, 7($sp)
+    sw $t8, 6($sp)
+    sw $ra, 5($sp)
+    sw $t5, 4($sp)
+    sw $t3, 3($sp)             
+    sw $t0, 2($sp)              
+    sw $t1, 1($sp)               
+    sw $t2, 0($sp)
+    addi $a0, $t8, 0  # pass the pointer to dictionary m_word character
+    jal bit_masking_dictionary_parsing
+    lw $t6, 7($sp)
+    lw $t8, 6($sp)
+    lw $ra, 5($sp)
+    lw $t5, 4($sp)
+    lw $t3, 3($sp)             
+    lw $t0, 2($sp)              
+    lw $t1, 1($sp)               
+    lw $t2, 0($sp)
+    addi $sp, $sp, 8
+    addi $t9, $v0, 0
+    addi $t7, $v1, 0
+    and $t6, $t9, $t6                            # masking the loaded dictionary word
+
+    addi $sp, $sp, -3
+    sw $ra, 2($sp)
+    sw $a0, 1($sp)
+    sw $a1, 0($sp)
+    addi $a0, $t7, 0
+    addi $a1, $t6, 0
+    jal sra_variable                                 # shift bits to the right, t6 should contain the ascii of char in dict
+    addi $t6, $v0, 0
+    lw $a1, 0($sp)
+    lw $a0, 1($sp)
+    lw $ra, 2($sp)
+    addi $sp, $sp, 3
+
+    addi $sp, $sp, -3
+    sw $ra, 2($sp)            
+    sw $t0, 1($sp)              
+    sw $t1, 0($sp)               
+
+    addi $a0, $t6, 0
+    jal check_in_range
+            
+    lw $ra, 2($sp)              
+    lw $t0, 1($sp)               
+    lw $t1, 0($sp)
+    addi $sp, $sp, 3
+
+    addi $t7, $zero, 1                                # indicates in range
+    bne $v0, $t7, reflect_adding_matched              # if not in range, this indicates that dictionary has also finished matching 
+                                                      # all its characters
+
+    addi $t1, $t1, 1
+    j compute_result 
+
+reflect_adding_matched:
+    addi $t1, $t1, 1
+    addi $t0, $t0, 1
+    j compute_result
+
+process_char:  
     addi $sp, $sp, -6
     sw $ra, 5($sp)
     sw $t5, 4($sp)
@@ -63,7 +137,78 @@ process_char:
     lw $t2, 0($sp)
     addi $sp, $sp, 6
     bne $v0, $zero, match_to_dictionary
-    j update_stats                            # all characters in previous decrypted word matched to a word inthe dictionary
+    nop
+    nop
+
+    # This indicates the input string word is at a delimiter at this point    
+    lwDict $t6, $t4, 0
+    nop
+    nop
+
+    addi $sp, $sp, -8
+    sw $t6, 7($sp)
+    sw $t8, 6($sp)
+    sw $ra, 5($sp)
+    sw $t5, 4($sp)
+    sw $t3, 3($sp)             
+    sw $t0, 2($sp)              
+    sw $t1, 1($sp)               
+    sw $t2, 0($sp)
+    addi $a0, $t8, 0  # pass the pointer to dictionary m_word character
+    jal bit_masking_dictionary_parsing
+    lw $t6, 7($sp)
+    lw $t8, 6($sp)
+    lw $ra, 5($sp)
+    lw $t5, 4($sp)
+    lw $t3, 3($sp)             
+    lw $t0, 2($sp)              
+    lw $t1, 1($sp)               
+    lw $t2, 0($sp)
+    addi $sp, $sp, 8
+    addi $t9, $v0, 0
+    addi $t7, $v1, 0
+
+
+    and $t6, $t9, $t6                            # masking the loaded dictionary word
+
+    addi $sp, $sp, -3
+    sw $ra, 2($sp)
+    sw $a0, 1($sp)
+    sw $a1, 0($sp)
+    addi $a0, $t7, 0
+    addi $a1, $t6, 0
+
+    jal sra_variable                                 # shift bits to the right, t6 should contain the ascii of char in dict
+    addi $t6, $v0, 0
+
+    lw $a1, 0($sp)
+    lw $a0, 1($sp)
+    lw $ra, 2($sp)
+    addi $sp, $sp, 3
+
+    addi $sp, $sp, -3
+    sw $ra, 2($sp)            
+    sw $t0, 1($sp)              
+    sw $t1, 0($sp)               
+
+    addi $a0, $t6, 0
+
+    jal check_in_range
+            
+    lw $ra, 2($sp)              
+    lw $t0, 1($sp)               
+    lw $t1, 0($sp)
+    addi $sp, $sp, 3
+
+    addi $r20, $v0, 0 # for debugging purposes, indicates the number of characters
+
+
+    addi $t7, $zero, 1                                # indicates in range
+
+
+    # ####
+    bne $v0, $t7, update_stats                            # all characters in previous decrypted word matched to a word inthe dictionary
+    j skip_to_next_word_in_dictionary
 
 update_stats:
     addi $r16, $r16, 1             # for debugging purposes, indicates the number of characters matched
@@ -72,14 +217,18 @@ update_stats:
     addi $t2, $t5, 1               # Increment dictionary pointer
     addi $t5, $t2, 0
     addi $t4, $zero, 0             # start over in dictionary
-    addi $t8, $zero, 0
+    addi $t8, $zero, 1
+    addi $a2, $zero, 0
     j read_next_word
 
 match_to_dictionary:
-    addi $r20, $r20, 1             # for debugging purposes
     lwDict $t6, $t4, 0
+
     nop
     nop
+    add $r17, $t5, $zero
+    
+    add $r18, $zero, $t6
     addi $sp, $sp, -8
     sw $t6, 7($sp)
     sw $t8, 6($sp)
@@ -121,7 +270,7 @@ match_to_dictionary:
     j update_stats_no_match
 
 update_stats_no_match:
-    addi $17, $17, 1               # for debugging purposes
+    
     addi $t1, $t1, 1               # Increment total d_word count
     addi $t4, $zero, 0                        # this indicates that input string word is not found in the dictionary
     addi $t8, $zero, 1
@@ -149,18 +298,18 @@ move_to_next_decrypted_dword:
     lw $t1, 1($sp)               
     lw $t2, 0($sp)
     addi $sp, $sp, 6
-    addi $r22, $v0, 0              # for debugging purposes
+#    addi $r22, $v0, 0              # for debugging purposes
     bne $v0, $zero, move_to_next_decrypted_dword     # delimiter not reached
     j end_move_to_next_decrypted_word                      # delimiter reached
 
 end_move_to_next_decrypted_word:
     addi $t5, $t5, 1               # Move to the next character in the decrypted word
     addi $t2, $t5, 0
+    addi $a2, $zero, 0             # indicates matching complete (although it failed)
     j read_next_word
     
 
 continue_match:
-
     bne $t6, $t3, skip_to_next_word_in_dictionary     # characters don't match which indicates this word in dictionary does not match
     j setup_check_next_dictionary_character
 
@@ -172,7 +321,8 @@ setup_check_next_dictionary_character:
 increment_and_match_to_dictionary:
     addi $t8, $t8, 1
     addi $t5, $t5, 1
-    addi $r19, $r19, 1                                # for debugging purposes, indicates the consecutive character matches
+    addi $r19, $t6, 0                                # for debugging purposes, indicates the consecutive character matches
+    addi $a2, $zero, 1                                # indicates matching in progress
     j read_next_word
     
 bit_masking_dictionary_parsing:
@@ -220,6 +370,8 @@ skip_to_next_word_in_dictionary:
 
 continue_skip_to_next_word_in_dictionary:
     addi $t8, $t8, 1                                  # update pointer to next character in dictionary
+    nop
+    nop
     lwDict $t6, $t4, 0
     # some shit going on with the bypassing here
     # can't fix, noping for now
@@ -266,11 +418,25 @@ continue_skip_to_next_word_in_dictionary:
     lw $ra, 2($sp)
     addi $sp, $sp, 3
 
-    addi $t7, $zero, 2                                # delimiter
-    bne $t6, $t7, skip_to_next_word_in_dictionary     # if not delimiter, move to next word in dict
+    addi $sp, $sp, -3
+    sw $ra, 2($sp)            
+    sw $t0, 1($sp)              
+    sw $t1, 0($sp)               
+
+    addi $a0, $t6, 0
+    jal check_in_range
+            
+    lw $ra, 2($sp)              
+    lw $t0, 1($sp)               
+    lw $t1, 0($sp)
+    addi $sp, $sp, 3
+
+    addi $t7, $zero, 0                                # indicates in range
+    bne $v0, $t7, skip_to_next_word_in_dictionary     # if not delimiter, move to next word in dict
 
     addi $t8, $t8, 1                                  # point to next character in dict
     addi $t5, $t2, 0                                  # reset character pointer to start of word in encrypted message
+    addi $a2, $zero, 1                                # indicates matching incomplete
     j read_next_word
 
 # inputs: a0 = shamt, a1 = register shifted
@@ -314,10 +480,10 @@ check_in_range:
 
 check_lower_case:
     # Check if ASCII is in the range of 'a' to 'z' (97-122)
-    addi $t2, $zero, 97               # Load ASCII for 'a'
-    addi $t3, $zero, 122              # Load ASCII for 'z'
-    blt $a0, $t2, check_in_range_return # If $a0 < 97, return not in range
-    blt $t3, $a0, check_in_range_return # If $a0 > 122, return not in range
+    addi $t0, $zero, 97               # Load ASCII for 'a'
+    addi $t1, $zero, 122              # Load ASCII for 'z'
+    blt $a0, $t0, check_in_range_return # If $a0 < 97, return not in range
+    blt $t1, $a0, check_in_range_return # If $a0 > 122, return not in range
 
 in_range:
     addi $v0, $zero, 1       # Set $v0 to 1 (in range)
